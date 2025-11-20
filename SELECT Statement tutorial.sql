f9d5bb1e605253c39129af4f7d7b34086bff5a04
shreyvyas125 <shreyvyas125@gmail.com>
2025-11-20 16:53:06 +0100

    Add files via upload

---
+++ SELECT Statement tutorial.sql
@@ -0,0 +1,15 @@
+select * 
+FROM employee_demographics;
+
+
+SELECT first_name,
+last_name,
+age,
+(age+10)*100 
+ FROM  employee_demographics;
+ 
+ SELECT age FROM employee_demographics;
+ 
+ select distinct first_name,last_name,gender From employee_demographics;
+ 
+ # PEMDAS
